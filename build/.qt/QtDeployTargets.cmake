97bcc96fdfb7c6dddbbfbe17629a7ed532365c55
Polisetti Veera Venkata Vijaya Subhash <49185065+subhashpolisetti@users.noreply.github.com>
2025-06-09 18:42:17 -0700

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_SecurePay_FILE /Users/subhashpolisetti/Documents/Visual Studio Workspaces/Software System Engienering Assignment-5/build/SecurePay)
-set(__QT_DEPLOY_TARGET_SecurePay_TYPE EXECUTABLE)
--- build/CMakeFiles/SecurePay.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SecurePay.
-# This may be replaced when dependencies are built.
